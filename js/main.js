7e4600425a323991a4396677f8382f8a9ab3f406
Emanuel Ramirez <Emmadc06@gmail.com>
2023-01-31 21:38:26 -0300

    Ultimo Dia

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+function tema() {
+    document.body.classList.toggle("dark");
+}
+
+function info() {
+    document.getElementById("info").style.display='block';
+}
+
+function sobremi() {
+    document.getElementById("sobremi").style.display='block';
+}
+
+function compo() {
+    document.getElementById("compo").style.display="block";
+}
+
+function objetivo() {
+    window.alert("Desarrollarme como profesional consiguiendo experiencia laboral en el rubro de la Programacion")
+}
+
+function bjava() {
+    window.alert("Utilice funciones JavaScript en los botones para entender su uso.");
+}
